ace260de2a77f6026b00a2f90fdc1e0fa0cb5c2e
franciscadiaz <77750417+franciscadiaz@users.noreply.github.com>
2021-02-16 19:55:49 +0100

    Add files via upload

---
+++ Suma.py
@@ -0,0 +1,18 @@
+"""
+1. inicio
+2. introducir y leer número entero
+3. introducir y leer otro número entero
+4. sumar numeros paso 2 + 3
+5. la suma de los dos números es
+6. fin
+"""
+
+print ("Voy a hacer sumas por tí")
+n1= int(input ("Introduce un número entero:"))
+n2= int(input ("Introduce otro número entero:"))
+"""
+se puede declarar el int bien en la variable  como en las lineas 11 y 12 
+o bien  en la linea 16.  suma = int (n1)+ int (n2)
+"""
+suma =  n1 + n2
+print ("La suma de los dos números es:" + str (suma))
---
+++ triangulo.py
@@ -0,0 +1,5 @@
+print ("Calcula el área de un triángulo")
+base=input("Cual es la base:")
+altura=input ("Cual es la altura:")
+area=int (base) * int (altura)/2
+print ("El resultado es: " + str (area))
